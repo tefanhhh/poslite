64a909f220e8186a2fb6d5e485ece1f54ff37355
Tefan Haetami <tefanhaetami87@gmail.com>
2022-08-11 22:34:52 +0700

    feat: csrf protection

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
+  app.use(csurf());
   await app.listen(3000);
 }
 bootstrap();
