94184105116ab927540e95f567263408da68731b
Tefan Haetami <tefanhaetami87@gmail.com>
2022-08-13 20:52:00 +0700

    feat: configuring tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '..', 'src', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'src', 'views'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
   app.enableCors();
   app.use(helmet());
