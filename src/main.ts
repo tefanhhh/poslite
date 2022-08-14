ea9054f08489fae61064acdab9f41442fa5adb5a
Tefan Haetami <tefanhaetami87@gmail.com>
2022-08-14 09:58:15 +0700

    feat: dynamic partials

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,22 @@ import helmet from 'helmet';
 // import * as csurf from 'csurf';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as handlebars from 'hbs';
+import * as fs from 'fs';
+
+function registerComponents() {
+  const dir = join(__dirname, '..', 'views', 'components');
+  const filenames = fs.readdirSync(dir);
+  filenames.forEach((filename: string) => {
+    const matches = /^([^.]+).hbs$/.exec(filename);
+    if (!matches) {
+      return;
+    }
+    const name = matches[1];
+    const template = fs.readFileSync(dir + '/' + filename, 'utf8');
+    handlebars.registerPartial(name, template);
+  });
+}
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -13,6 +29,7 @@ async function bootstrap() {
   app.enableCors();
   app.use(helmet());
   // app.use(csurf());
+  registerComponents();
   await app.listen(3000);
 }
 bootstrap();
